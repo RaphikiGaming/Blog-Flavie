e38b276c93bff27419469b92a29a61599eb6f489
RaphikiGaming <64139060+RaphikiGaming@users.noreply.github.com>
2021-02-13 20:49:09 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var largeur = window.innerWidth;
 var hauteur = window.innerHeight;
 
-var test = 0;
 if (largeur <= hauteur) {
 	document.getElementById("stylesheet").setAttribute("href", "portrait.css");
 } else {
