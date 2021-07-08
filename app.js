2d9921e2218878b647aa4fd6cf252832c1a75501
RaphikiGaming <64139060+RaphikiGaming@users.noreply.github.com>
2021-07-08 20:44:44 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -3,6 +3,25 @@ var hauteur = window.innerHeight;
 
 if (largeur <= hauteur) {
 	document.getElementById("stylesheet").setAttribute("href", "portrait.css");
+	alert("Si vous souhaitez changer l'orientation de la page, actualisez la page après avoir changé l'orientation de votre appareil")
 } else {
 	document.getElementById("stylesheet").setAttribute("href", "style.css");
+}
+
+var slide = 1;
+var duree = 6000;
+
+if(slide == 1){
+	setTimeout(toSlide2, duree);
+}
+
+function toSlide1(){
+	document.querySelector(".slide1").style.left = "0%";
+	document.querySelector(".slide2").style.left = "100%";
+	setTimeout(toSlide2, duree);
+}
+function toSlide2(){
+	document.querySelector(".slide1").style.left = "-100%";
+	document.querySelector(".slide2").style.left = "0%";
+	setTimeout(toSlide1, duree);
 }
